6a42942aa3fd3d38ec40d865a922ed10e0d9cd84
Denis <fordz2012@gmail.com>
2020-09-03 15:07:53 +0300

    m7-t2 Доработки 1.2

--- source/js/modal-opened.js
+++ source/js/modal-opened.js
@@ -1,13 +1,20 @@
 var formModal = document.querySelector('.modal-basket');
 var buttonModalOpen = document.querySelector('.product-cart__button');
+var bgOverlay = document.querySelector('.overlay');
+var buttonModalClose = document.querySelector('.modal-basket__button');
 
 buttonModalOpen.addEventListener('click', function(evt) {
   if (!formModal.classList.contains('modal-basket--opened')) {
-     formModal.classList.add('modal-basket--opened');
+    evt.preventDefault();
+    formModal.classList.add('modal-basket--opened');
+    bgOverlay.classList.add('overlay--opened');
   }
+})
 
-  else {
+buttonModalClose.addEventListener('click', function(evt) {
+  if (formModal.classList.contains('modal-basket--opened')) {
+    evt.preventDefault();
     formModal.classList.remove('modal-basket--opened');
+    bgOverlay.classList.remove('overlay--opened');
   }
-
 })
