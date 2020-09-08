e7b4801c4d7d65073b536982b1775e008c828455
Denis <fordz2012@gmail.com>
2020-09-08 11:35:01 +0300

    m8-t2 Автоматизация 1.4

--- gulpfile.js
+++ gulpfile.js
@@ -8,101 +8,112 @@ const sync = require("browser-sync").create();
 const csso = require("gulp-csso");
 const rename = require("gulp-rename");
 const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
-const webp = require('gulp-webp');
 const del = require("del");
-// Styles
+
+// clean
+const clean = () => {
+  return del("build");
+}
+exports.clean = clean;
+
+// copy
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+}
+
+// styles
 const styles = () => {
-return gulp.src("source/sass/style.scss")
-.pipe(plumber())
-.pipe(sourcemap.init())
-.pipe(sass())
-.pipe(gulp.dest("./build/css"))
-.pipe(postcss([
-autoprefixer()
-]))
-.pipe(csso())
-.pipe(rename("styles.min.css"))
-.pipe(sourcemap.write("."))
-.pipe(gulp.dest("./build/css"))
-.pipe(sync.stream());
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(gulp.dest("build/css"))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream());
 }
 exports.styles = styles;
-// HTML
+
+
+// html
 const html = () => {
-return gulp.src("source/*.html")
-.pipe(gulp.dest("./build"))
-.pipe(sync.stream())
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("build"))
+    .pipe(sync.stream());
 }
 exports.html = html;
-//Imagemin
+
+// image
 const images = () => {
-return gulp.src("source/img/**/*.{jpg,png,svg}")
-.pipe(imagemin([
-imagemin.optipng({optimizationLevel: 3}),
-imagemin.mozjpeg({progressive: true}),
-imagemin.svgo()
-]))
-.pipe(gulp.dest("./build/img"))
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({ progressive: true }),
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.svgo()
+    ]))
 }
 exports.images = images;
+
 // webp
-const makeWebp = () => {
-return gulp.src("source/img/**/*.{png,jpg}")
-.pipe(webp({quality: 90}))
-.pipe(gulp.dest("./build/img"))
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("build/img"))
 }
-exports.makeWebp = makeWebp;
-// Sprite
+exports.webp = createWebp;
+
+// sprite
 const sprite = () => {
-return gulp.src("source/img/**/icon-*.svg")
-.pipe(svgstore())
-.pipe(rename("sprite.svg"))
-.pipe(gulp.dest("build/img"))
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
 }
 exports.sprite = sprite;
-// Copy
-const copy = () => {
-return gulp.src([
-"source/fonts/**/*.{woff,woff2}",
-"source/js/**",
-"source/*.ico"
-], {
-base: "source"
-})
-.pipe(gulp.dest("build"))
-};
-exports.copy = copy;
-// Clean
-const clean = () => {
-return del("build");
-};
-exports.clean = clean;
-// Server
-const server = done => {
-sync.init({
-server: {
-baseDir: './build'
-},
-cors: true,
-notify: false,
-ui: false,
-});
-done();
+
+// server
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false
+  });
+  done();
 }
+
 exports.server = server;
-// Watcher
+
+// watcher
 const watcher = () => {
-gulp.watch("source/sass/**/*.scss", gulp.series(styles));
-gulp.watch("source/*.html", gulp.series(html));
+  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html", gulp.series("html"));
 }
-// Build
-const build = gulp.series(clean, copy, styles, images, makeWebp ,sprite, html);
+
+// build
+const build = gulp.series(
+  clean, copy, styles, sprite, html
+);
+
 exports.build = build;
 
-// Start
-exports.start = gulp.series(build, server, watcher);
-
-//Default
-exports.default = gulp.series(build, server, watcher);
-
+exports.default = gulp.series(
+  build, server, watcher
+);
